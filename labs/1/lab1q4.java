afb80f13037df6e6bfdad6c5a135732484cdbe21
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-06 16:53:38 -0400

    lab 1 question 4

---
+++ labs/1/lab1q4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+	class Energy{
+		public static void main{
+		Scanner input = new Scanner(System.in);
+		system.out.print("Enter a mass: ");
+		double mass = input.mass();
+		int energy = (int) (mass * Math.pow(299972458, 2));
+		system.out.print("The Energy is: ", energy);
+	}
+}
