8bc9c4d2c895dbd0f736ffb9433afbd932b259a8
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-22 19:23:29 -0400

    largest prime number

---
+++ labs/3/primeCalc.java
@@ -0,0 +1,23 @@
+import java.until.Scanner;
+
+
+public class primeCalc {
+	public static void main(String[] args);
+		boolean isPrime(int num){
+			for(int i = 2; i < num; i++){
+				if(num % i == 0){
+					return false;
+				}
+			}
+			return true;
+		int less = 15000;
+		for(int i = 15000; i < num; i--) {
+			if(num == less) {
+				print("The largest prime number is " + less);
+			}
+			}
+				
+		}
+		}
+
+}
