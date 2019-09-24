e5e16bfb5417bb564bd80dff9adc2b1f4a1069d6
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-23 23:00:56 -0400

    vowel count in a string

---
+++ labs/3/vowelCount.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class vowelCount {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter some letters: ");
+		String letters = in.nextLine();
+	}
+	public static int numVowels(String letters) {
+		int count = 0;
+		for(int i = 0; i < letters.length(); i ++) {
+			if(letters.charAt(i) == 'a' || letters.charAt(i) == 'e' || letters.charAt(i) == 'i' || letters.charAt(i) == 'o' || letters.charAt(i) == 'u' ) {
+				count ++;
+			}
+		}
+		return count;
+		System.out.print("There are " + numVowels(letters) + " vowels in those letters.");
+	}
+}
