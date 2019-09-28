0b315e02821c82b9abf851d082263eb8a62b2782
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-27 22:20:32 -0400

    check if a string is the same in reverse

---
+++ labs/4/isPalindrome.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class isPalindrome {
+	public static void main(String[] arg) {
+		String str, reverse = "";
+	    Scanner sc = new Scanner(System.in);
+	 
+	    System.out.println("Enter a string:");
+	    str = sc.nextLine();
+	 
+	    int length = str.length();
+	public static String reverse(String s){
+	    for ( int i = length - 1; i >= 0; i-- )
+	    rev = rev + str.charAt(i);
+	    	
+	public static boolean isPalindrome(String s) {
+	    if (str.equals(reverse))
+	    	isPalindrome = true;
+	     	System.out.println(str+" is a palindrome");
+	    else
+	    	isPalindrome = false;
+	     	System.out.println(str+" is not a palindrome");
+	    }
+	  }
+	 
+	}
+}
