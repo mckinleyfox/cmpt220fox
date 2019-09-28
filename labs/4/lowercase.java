df76f4333af38ea41f737c269ff4dbb91c912f57
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-27 23:30:08 -0400

    random matrix

---
+++ labs/4/lowercase.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lowercase {
+	public static void main(String[] args) {
+		char[][] alph =  new char [5][5];
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j<5; j++) {
+				int matrix = (int)((Math.random());
+				alph[i][j] = (char)(matrix);
+				System.out.print(alph[i][j] + " ");
+				
+			}
+		
+		}
+		
+	}
+}
