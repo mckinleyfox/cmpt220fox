21536b268dda3e43017807b4caf7e3835baf9839
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-09-28 20:15:04 -0400

    deleting duplicates of an array

---
+++ labs/3/duplicate.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Array;
+import java.util.Scanner
+
+public class duplicate {
+
+	public static int deleteDup(int array[], int a) {
+		int delete(int array[], int a){
+		}
+		if(a == 0 || a ==1) {
+			return a;
+		int[] num = new int[a];
+		int j =0;
+		}
+		for(int i =0; i<n-1; i++) {
+			if(array[i] != array[i+1]) {
+				num[j++] = array[i];
+			}
+		for(int i = 0; i<j; i ++) {
+			array[i] = num[i];
+			return j;
+		}
+		}
+
+	}
+	public static void main(String[] args) {
+		System.out.println("Enter some numbers: ");
+		int length = Array.getLength(array);
+		for (int i = 0; i< length; i++) {
+			System.out.println(" " + array[i]);
+		}
+	}
+
+}
