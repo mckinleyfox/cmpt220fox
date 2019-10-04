6a18bf61459b9be3eb3be3fd646894c44c119c69
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-10-03 20:46:44 -0400

    finds the largest element in an array

---
+++ labs/5/largestElement.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class largestElement {
+
+	public static void main(String[] args) {
+		Scanner input = new Scannner(System.in);
+		System.out.println("Enter 10 numbers: ");
+		double[] a = new double[10];
+		a[0] = input.nextDouble();
+		a[1] = input.nextDouble();
+		a[2] = input.nextDouble();
+		a[3] = input.nextDouble();
+		a[4] = input.nextDouble();
+		a[5] = input.nextDouble();
+		a[6] = input.nextDouble();
+		a[7] = input.nextDouble();
+		a[8] = input.nextDouble();
+		a[9] = input.nextDouble();
+	}
+	
+	public static double max(double[] a) {
+		double max = a[0];
+		for(int i = 0; i< a.length; i++) {
+			if(a[i]> max) {
+				max = a[i];
+			}	
+		}
+		return max;
+		System.out.println("The maximum value is: " + max);
+		
+	}
+
+}
