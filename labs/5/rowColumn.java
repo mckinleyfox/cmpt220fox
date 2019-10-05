4cb8512641f048da227bf52fe03c66d76f434d2e
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-10-04 21:53:30 -0400

    transpose of an array

---
+++ labs/5/rowColumn.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class rowColumn {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the size of the square matrix: ");
+		int row = nextInt();
+		int column = nextInt();
+		int matirx[][] = new int[row][column];
+		System.out.println("Enter the values of the matrix: ");
+		int values = input.nextInt();
+		for(int i = 0; i <row; i++) {
+			for(int j = 0; j<column; j++) {
+				matirx[i][j] = nextInt();
+				System.out.print(" ");
+			}
+		}
+		for(int i = 0; i < column; i++){
+      	    for(int j = 0; j < row; j++){
+      	    	System.out.println("The transpose of the matrix is: " + matrix[j][i])
+      	    }
+
+	}
+
+}
