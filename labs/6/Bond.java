1f923f906fd1fe767a45366e0d2a0bbac3b3541d
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-10-18 21:46:20 -0400

    financial bond

---
+++ labs/6/Bond.java
@@ -0,0 +1,29 @@
+
+public class Bond {
+	public static void main(String[] args) {
+		Bond test = new Bond(500, 100, .05, 10,000);
+		System.out.println("The price of the bond is: " + test.getPrice());
+	}
+	private double coupon;
+	private int payments;
+	private double interest;
+	private double valueMaturity;
+	Bond(){
+		coupon = 0;
+		payments = 0;
+		interest = 0;
+		valueMaturity = 0;
+		}
+	Bond(double c, int n, double i, double M){
+		coupon = c;
+		payments = n;
+		interest = i;
+		valueMaturity = m;
+	}
+	public static double getPrice() {
+		double price;
+		price = (c * ( 1-(1/Math.pow(1+i, n)))) + (m*(1/Math.pow(1+i, n)));
+		return price;
+	}
+	
+}
