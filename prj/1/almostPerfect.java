f5e60376ac62e65dbd19de1da3d95d2f16690239
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-10-23 23:14:03 -0400

    almost perfect problem

--- prj/1/almostPerfect.java
+++ prj/1/almostPerfect.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class almostPerfect {
 	public static void main(String[] args) {
 		int sum = 0;
-		int num = 0;
-		sc = new Scanner(Scanner.in);
-		System.out.println("\n Enter a number: ");
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		double num = num.nextInt;
 		for(int i = 1; i<num; i++) {
 			if(num%i==0) {
 				sum = sum +1;
 			}
 		}
 		if(sum == num) {
-			System.out.println(num, "Perfect");
+			System.out.println(num + "Perfect");
 		}
-		if (sum == num +1) {
-			System.out.println(num, "Almost Perfect");
+		if (sum == num + 1) {
+			System.out.println(num + "Almost Perfect");
 		}
 		else
-			System.out.println(num, "Not perfect");
+			System.out.println(num + "Not perfect");
 
 	}
 
