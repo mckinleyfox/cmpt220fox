13c26524d333baf88abf6024d673925dfd31aa8f
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-08 18:03:04 -0500

    exception for an illegal triangle

---
+++ labs/9/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+
+public class IllegalTriangleException {
+	public IllegalTriangleException(String s) {
+		System.out.println("This is an illegal triangle because the sum of "
+				+ "two sides is greater than the other side.");
+	}
+}
