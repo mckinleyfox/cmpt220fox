118784fe7ad68bb0c215a9902a13a42e3137dd5a
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-08 20:43:04 -0500

    removes occurrences from a string in a file

---
+++ labs/9/RemoveString.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class RemoveString {
+	public static void main(String[] args) {
+		if(args.length != 2) {
+			System.out.println("RemoveString <Stirng> <file>");
+			System.exit(0);
+		}
+	} try {
+		Scanner input = new Scanner(new File(args[1]));
+		while(input.hasNextLine()) {
+			String line = input.nextLine();
+			line = line.replaceAll(args[0],  "");
+		}
+		input.close();
+	}catch(FileNotFoundException ex) {
+		System.out.println("Could not open file");
+	}
+	}	
+}
