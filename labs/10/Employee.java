1579201378ce6a86ce477de5cfb40684084548ae
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-14 23:03:48 -0500

    employee abstract class

---
+++ labs/10/Employee.java
@@ -0,0 +1,17 @@
+
+public abstract class Employee {
+	String name;
+	double age;
+	double hourRate;
+	public Employee(String name, double age, double hourRate) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.hourRate = hourRate;
+	}
+	public abstract double salary(double hours);
+	@Override
+	public String toString() {
+		return "name = " + name + ", age = " + age + ", hourly raate" + hourRate;
+	}
+}
