77689a5a192a593004935e2fd046bdadd8ee1b87
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-14 23:04:26 -0500

    manager class extending employee

---
+++ labs/10/Manager.java
@@ -0,0 +1,10 @@
+
+public class Manager extends Employee {
+	public Manager(String name, double age, double hourRate) {
+		super(name, age, hourRate);
+	}
+	@Override
+	public double salary(double hours) {
+		return hourRate * hours;
+	}
+}
