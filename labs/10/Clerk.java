ddf3928dd317fcaa9ff0bd1c00565dc338714a72
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-14 23:05:00 -0500

    clerk class extending employee

---
+++ labs/10/Clerk.java
@@ -0,0 +1,11 @@
+
+public class Clerk extends Employee{
+	public Clerk(String name, double age, double hourRate) {
+		super(name, age, hourRate);
+	}
+	@Override
+	public double salary(double hours) {
+		return hourRate * hours;
+	}
+}
+
