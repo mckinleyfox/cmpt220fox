0fad2498564628b37e93a1d0eb0a00dd415638df
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-11-15 23:52:10 -0500

    application of appliance class

---
+++ labs/10/ApplianceApp.java
@@ -0,0 +1,19 @@
+package lab10q4;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ApplianceApp extends Appliance {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Random num = new Random();
+		int length = 40;
+		int[] array = new int[length];
+		for(int i = 0; i < length; i++) {
+			array[i] = num.nextInt(40);
+	}
+	array.sort(appliance);
+	for(Appliance a.appliance) {
+		System.out.println(a);
+	}
+}
