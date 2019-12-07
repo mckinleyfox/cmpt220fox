e99ad84459cf2632a1a41e3eb53fb1d347b72bf2
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-12-06 22:13:01 -0500

    methods

---
+++ prj/2/code/List.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+
+public class List {
+	private String task;
+	private boolean completed;
+	private String dateCreated;
+	
+	public List(String t) {
+		t = task;
+		completed = false;
+	}
+	
+	public String getT() {
+		return T;
+	}
+	
+	public boolean isCompleted() {
+		return completed;
+	}
+	
+	public String getDateCreated() {
+		return dateCreated;
+	}
+	
+}
