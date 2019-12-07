045f59f433d04d7ba3a242610add6457de5fd3fd
mckinleyfox <47337991+mckinleyfox@users.noreply.github.com>
2019-12-06 22:13:26 -0500

    random reward saying

---
+++ prj/2/code/Reward.java
@@ -0,0 +1,8 @@
+
+public class Reward {
+	public static void main(String[] args) {
+		String[] rewards = {"Congrats! You did it!", "Yay! Go you!", "Great Work!", "You're doing great!",
+				"Keep up the good work!"};
+		}
+	}
+
